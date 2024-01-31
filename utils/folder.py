8b396c4954db929047aa910a9a073f207b9a6732
= <jirobassik@tut.by>
2024-01-31 10:37:29 +0300

    Fix add folder, check perm commands

--- utils/folder.py
+++ utils/folder.py
@@ -1,29 +1,18 @@
 import os
 import pathlib
-import stat
 import subprocess
 
 import click
 
 
-def create_folder(path, mode):
-    # pathlib.Path(f"./{path}").mkdir(mode=0o707)
-    # folder_stat = os.stat(path)
-    # folder_mode = folder_stat.st_mode & 0o777
-    # print(folder_mode)
-    # print(str(pathlib.Path(f"./{path}").absolute().resolve()))
-    # subprocess.run(['chmod', '707', str(pathlib.Path(f"./{path}/").absolute())])
-    # print(mode)
-    # os.umask(0)
-    # os.makedirs(f"./{path}", exist_ok=True)
-    # os.chmod(f"./{path}", 0o707)
-    original_umask = os.umask(0)
-    try:
-        os.makedirs(f"./{path}", 0o40444)
-    finally:
-        os.umask(original_umask)
+def create_folder(path, mode, sid):
+    folder_path = str(pathlib.Path(f"./{path}").absolute())
+    os.makedirs(folder_path)
+    subprocess.run(['icacls', folder_path, '/inheritance:r'])
+    subprocess.run(['icacls', folder_path, '/grant', f'*S-1-{sid}:{mode}'])
 
 
 def folder_permission(path):
-    perm = pathlib.Path(f"./{path}").stat().st_mode
-    click.echo(f'Permission for folder "{path}": {oct(perm)} ({stat.filemode(perm)})')
+    result = subprocess.run(['icacls', f"./{path}", '/t'], capture_output=True, text=True, encoding='cp866')
+    output = result.stdout
+    click.echo(output)
